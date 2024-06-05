a96a731530bd506de189cfc09e8daa6666f6865b
VladKlochkov <vova7777159@gmail.com>
2024-06-05 17:22:56 +0300

    Добавлен вывод параметров структуры человека

---
+++ Project4.5.6.1/PrintPars.cpp
@@ -0,0 +1,7 @@
+#include "PrintPars.h"
+
+void printPersonPars(Person p)
+{
+	cout << "Фио: " << p.name << " " << p.secondname << " " << p.surname << "." << endl;
+	cout << "Возраст: " << p.age << endl;
+}
---
+++ Project4.5.6.1/PrintPars.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Struct_Person.h"
+using namespace std;
+
+void printPersonPars(Person p);
